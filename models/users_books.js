f8328b01d9d28a78b08ed18f1d8fb601e1ef4c6e
Alejandro Tinoco <alejandrodxwwe@hotmail.com>
2021-05-21 14:00:36 -0500

    Delete comments

--- models/users_books.js
+++ models/users_books.js
@@ -2,14 +2,7 @@
 const { Model } = require("sequelize");
 module.exports = (sequelize, DataTypes) => {
   class Users_Books extends Model {
-    /**
-     * Helper method for defining associations.
-     * This method is not a part of Sequelize lifecycle.
-     * The `models/index` file will call this method automatically.
-     */
-    static associate(models) {
-      // define association here
-    }
+    static associate(models) {}
   }
   Users_Books.init(
     {
@@ -23,4 +16,3 @@ module.exports = (sequelize, DataTypes) => {
   );
   return Users_Books;
 };
-
